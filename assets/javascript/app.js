690d487c9c63dc7af4d200a6eb413d56fb78a46a
Jessie S <jessie.sholler@gmail.com>
2019-07-29 19:41:28 -0400

    basic HTML / css and javascript set up

---
+++ assets/javascript/app.js
@@ -0,0 +1,173 @@
+/*
+========================================
+Trivia Game - Questions & Answers 
+========================================
+*/
+
+
+
+/*
+========================================
+GLOBAL VARIABLES 
+========================================
+*/
+var correct = 0;   // Questions Answered Correctly
+var wrong = 0;    // Questions Answered Wrong
+var na = 0;      // Questions Not Answered 
+
+var time = 0;   // Countdown clock
+
+
+
+var crazyLaws = [{ 
+            // Question 1
+            question: "...let your donkey sleep in a bathtub?",
+            rightAnswer: "Arizona",
+            options: ["Oklahoma", "California", "Nebraska", "Arizona"],
+           
+            }, {
+            // Question 2
+            question: "...ride your horse above 10mph?",
+            rightAnswer: "Indiana",
+            options: ["Indiana", "Pennsylvania", "Kentucky", "Illinois"],
+
+            }, {
+            // Question 3
+            question: "...blow your ‘Bean’ whistle?",
+            rightAnswer: "Connecticut",
+            options: ["Connecticut", "Massachusetts", "Rhode Island", "Main"],
+            
+            }, {
+            // Question 4
+            question: "...honk your horn near a sandwich shop after 9 p.m?",
+            rightAnswer: "Arkansas",
+            options: ["Virginia", "Arkansas", "Colorado", "Utah"],
+
+            }, {
+            // Question 5
+            question: "…send your bestfriend a surprise pizza?",
+            rightAnswer: "Louisiana",
+            options: ["Maryland", "Montana", "Louisiana", "Ohio"],
+            }, {
+            // Question 6
+            question: "...steal an  alligator?",
+            rightAnswer: "Louisiana",
+            options: ["Florida", "Alabama", "Louisiana", "South Carolina"],
+
+            }, {
+            // Question 7
+            question: "...sing in the street after 11pm?",
+            rightAnswer: "Kansas",
+            options: ["Tennessee", "Kansas", "New Jersey", "Illinois"],
+
+            }, {
+            // Question 8
+            question: "...share your Netflix password?",
+            rightAnswer: "Tennessee",
+            options: ["Texas", "Washington", "Georgia", "Tennessee"],
+
+            }, {
+            // Question 9
+            question: "...lay down on the sidewalk?",
+            rightAnswer: "Nevada",
+            options: ["Nevada", "New York", "Illinois", "Massachussets"],
+
+            }, {
+            // Question 10
+            question: "...sell junk to a drunk?",
+            rightAnswer: "Wyoming",
+            options: ["California", "Oregon", "Wyoming", "Iowa"],
+
+            }, {
+            // Question 11
+            question: "...predict the future?",
+            rightAnswer: "Oregon",
+            options: ["Montana", "Oregon","New Mexico", "Alabama"],
+
+            }, {
+            // Question 12
+            question: "...kill Bigfoot / Sasquatch / Yeti?",
+            rightAnswer: "Washington",
+            options: ["Washington", "Alaska", "New Hampshire", "North Dakota"],
+        }]
+
+    console.log([crazyLaws]);
+    console.log(crazyLaws[0].question);
+    console.log(crazyLaws[0].rightAnswer);
+    console.log(crazyLaws[0].options);
+   
+    console.log(crazyLaws[1].question);
+    console.log(crazyLaws[1].rightAnswer);
+    console.log(crazyLaws[1].options);
+
+    console.log(crazyLaws[2].question);
+    console.log(crazyLaws[2].rightAnswer);
+    console.log(crazyLaws[2].options);
+
+    console.log(crazyLaws[3].question);
+    console.log(crazyLaws[3].rightAnswer);
+    console.log(crazyLaws[3].options);
+
+    console.log(crazyLaws[4].question);
+    console.log(crazyLaws[4].rightAnswer);
+    console.log(crazyLaws[4].options);
+    
+    console.log(crazyLaws[5].question);
+    console.log(crazyLaws[5].rightAnswer);
+    console.log(crazyLaws[5].options);
+
+    console.log(crazyLaws[6].question);
+    console.log(crazyLaws[6].rightAnswer);
+    console.log(crazyLaws[6].options);
+
+    console.log(crazyLaws[7].question);
+    console.log(crazyLaws[7].rightAnswer);
+    console.log(crazyLaws[7].options);
+
+    console.log(crazyLaws[8].question);
+    console.log(crazyLaws[8].rightAnswer);
+    console.log(crazyLaws[8].options);
+
+    console.log(crazyLaws[9].question);
+    console.log(crazyLaws[9].rightAnswer);
+    console.log(crazyLaws[9].options);
+
+    console.log(crazyLaws[10].question);
+    console.log(crazyLaws[10].rightAnswer);
+    console.log(crazyLaws[10].options);
+
+    console.log(crazyLaws[11].question);
+    console.log(crazyLaws[11].rightAnswer);
+    console.log(crazyLaws[11].options);
+
+/*
+========================================
+MEDIA
+========================================
+*/
+
+$(document).ready(function(){
+
+/*
+========================================
+START GAME - FROM INTRO PAGE 
+========================================
+*/
+
+/*
+========================================
+RESET GAME
+========================================
+*/
+ 
+
+/*
+========================================
+function 
+========================================
+*/
+
+
+
+
+}); 
